f4fa6be24b959200319f38ac36543e0d47768acb
skmirajbn <skmirajbn@gmail.com>
2023-10-10 22:55:46 +0600

    updated

--- Server/database/migrations/2023_10_10_150638_create_abouts_table.php
+++ Server/database/migrations/2023_10_10_150638_create_abouts_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
      * Run the migrations.
      */
     public function up(): void {
-        Schema::create('about', function (Blueprint $table) {
+        Schema::create('abouts', function (Blueprint $table) {
             $table->foreignId('user_id')->constrained('users', 'user_id');
             $table->text('about_bio');
             $table->timestamps();
@@ -20,6 +20,6 @@ return new class extends Migration {
      * Reverse the migrations.
      */
     public function down(): void {
-        Schema::dropIfExists('about');
+        Schema::dropIfExists('abouts');
     }
 };
