4620dbf5b712aea6464eb21709b622044c8db9bb
Sk Miraj <skmirajbn@gmail.com>
2023-10-10 12:05:26 +0600

    updated

--- Server/database/migrations/2023_10_10_060236_create_comments_table.php
+++ Server/database/migrations/2023_10_10_060236_create_comments_table.php
@@ -4,15 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('comments', function (Blueprint $table) {
-            $table->id();
+            $table->id('comment_id');
+            $table->integer('user_id');
+            $table->string('comment_text');
+            $table->integer('comment_likes');
             $table->timestamps();
         });
     }
@@ -20,8 +21,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('comments');
     }
 };
