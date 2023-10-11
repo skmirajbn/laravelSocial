d6abbb8949241c45fb7ccb0a17b762b974694a73
skmirajbn <skmirajbn@gmail.com>
2023-10-11 19:58:47 +0600

    updated

--- Server/app/Http/Controllers/About/AboutController.php
+++ Server/app/Http/Controllers/About/AboutController.php
@@ -27,25 +27,27 @@ class AboutController extends Controller {
     function put(Request $request) {
         $user = auth()->user();
         $id = $user['user_id'];
-        if ($request->expectsJson()) {
-            $bio = $request->input('bio');
-            echo $bio;
-            return;
-        }
+        $bio = $request->input('bio');
+
         if (!$user) {
             return response()->json(['message' => 'Unauthorized', 401]);
         }
 
         $about = About::where('user_id', $id)->first();
         if ($about) {
-            $about->update(['about_bio' => 'new value']);
+            $about = $about->update(['about_bio' => $bio]);
             if ($about) {
                 return response()->json(['message' => 'bio updated sucessfully']);
             } else {
                 return response()->json(['message' => 'bio update Failed']);
             }
         } else {
-            return response()->json(['message' => 'Not Found']);
+            $createAbout = About::create(['user_id' => $id, 'about_bio' => $bio]);
+            if ($createAbout) {
+                return response()->json(['message' => 'bio Created']);
+            } else {
+                return response()->json(['message' => 'bio Creation Failed']);
+            }
         }
 
     }
--- Server/app/Models/About.php
+++ Server/app/Models/About.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class About extends Model
-{
+class About extends Model {
     use HasFactory;
+
+    protected $primaryKey = 'user_id';
+    protected $fillable = [
+        'user_id',
+        'about_bio',
+    ];
 }
