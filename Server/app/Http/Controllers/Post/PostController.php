1811b2783850da4ac771bb8eb29f1bcb9c344a75
skmirajbn <skmirajbn@gmail.com>
2023-10-12 10:52:10 +0600

    updated

---
+++ Server/app/Http/Controllers/Post/PostController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Post;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    function get() {
+    }
+    function put(Request $request) {
+    }
+}
---
+++ Server/app/Models/Image.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Image extends Model {
+    use HasFactory;
+    protected $primaryKey = 'image_id';
+    protected $fillable = [
+        'image_id',
+        'post_id',
+        'image_name',
+    ];
+}
