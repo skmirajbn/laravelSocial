b248dbfa80556572929d63fd0b18a20461a06cd4
skmirajbn <skmirajbn@gmail.com>
2023-10-12 18:35:09 +0600

    updated

--- Server/app/Http/Controllers/Post/PostController.php
+++ Server/app/Http/Controllers/Post/PostController.php
@@ -9,28 +9,7 @@ class PostController extends Controller {
     function get() {
     }
     function put(Request $request) {
-
-        // $postId = $request->input('post_id');
-        // $postTitle = $request->input('post_title');
-        // $postBody = $request->input('post_body');
-        // $userId = $request->input('user_id');
-        // $createPost = Post::create([
-        //     'post_id' => $postId,
-        //     'post_title' => $postTitle,
-        //     'post_body' => $postBody,
-        //     'user_id' => $userId,
-        // ]);
-
-
-        // if ($createPost) {
-        //     return response()->json([
-        //         'message' => 'post Created'
-        //     ]);
-        // } else {
-        //     return response()->json([
-        //         'message' => 'post Creation failed'
-        //     ]);
-        // }
+        echo $request->input('post_body');
 
     }
 }
