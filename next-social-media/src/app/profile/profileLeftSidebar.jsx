0bb7700721279a4887aa2aecbd021af1e2edb199
skmirajbn <skmirajbn@gmail.com>
2023-10-15 00:49:48 +0600

    updated

--- next-social-media/src/app/profile/profileLeftSidebar.jsx
+++ next-social-media/src/app/profile/profileLeftSidebar.jsx
@@ -2,60 +2,62 @@ import ProfileIntro from "./profileIntro";
 
 export default function ProfileLeftSidebar(props) {
   return (
-    <div className="w-1/3 space-y-8 ">
-      <ProfileIntro />
-      <div className="shadow-lg shadow-gray-400 rounded-lg p-6 space-y-4">
-        <h3 className="text-2xl font-bold">Photos</h3>
-        <div className="grid grid-cols-3 gap-4">
-          <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
-          <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
-          <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
-          <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
-          <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
-          <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
-          <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
-          <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
-          <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
+    <div className="w-1/3 ">
+      <div className="space-y-8 sticky -top-60">
+        <ProfileIntro />
+        <div className="shadow-lg shadow-gray-400 rounded-lg p-6 space-y-4 ">
+          <h3 className="text-2xl font-bold">Photos</h3>
+          <div className="grid grid-cols-3 gap-4">
+            <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
+            <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
+            <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
+            <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
+            <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
+            <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
+            <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
+            <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
+            <img className="rounded-md h-24 w-24 object-cover mx-auto" src="img/profile.jpg" alt="" />
+          </div>
         </div>
-      </div>
-      <div className="shadow-lg shadow-gray-400 rounded-lg p-6 space-y-4">
-        <h3 className="text-2xl font-bold">Friends</h3>
-        <div className="grid grid-cols-3 gap-4">
-          <div className="flex flex-col justify-center items-center">
-            <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
-            <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
-            <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
-            <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
-            <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
-            <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
-            <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
-            <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
-            <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
-          </div>
-          <div className="flex flex-col justify-center items-center">
-            <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
-            <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
+        <div className="shadow-lg shadow-gray-400 rounded-lg p-6 space-y-4">
+          <h3 className="text-2xl font-bold">Friends</h3>
+          <div className="grid grid-cols-3 gap-4">
+            <div className="flex flex-col justify-center items-center">
+              <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
+              <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
+              <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
+              <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
+              <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
+              <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
+              <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
+              <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
+              <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
+            </div>
+            <div className="flex flex-col justify-center items-center">
+              <img className="rounded-md h-24 w-24 object-cover" src="img/profile.jpg" alt="" />
+              <h3 className="text-xs font-bold text-center">SM Jahangir Alam</h3>
+            </div>
           </div>
         </div>
       </div>
