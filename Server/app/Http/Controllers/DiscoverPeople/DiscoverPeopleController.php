2c40f0d50090b083be8afa852cc0a1e357c9dee6
skmirajbn <skmirajbn@gmail.com>
2023-10-17 17:39:10 +0600

    updated

--- Server/app/Http/Controllers/DiscoverPeople/DiscoverPeopleController.php
+++ Server/app/Http/Controllers/DiscoverPeople/DiscoverPeopleController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers\DiscoverPeople;
 
 use App\Http\Controllers\Controller;
+use App\Models\ProfileImage;
+use App\Models\User;
 
 class DiscoverPeopleController extends Controller {
     function get() {
         $user = auth()->user();
         $userId = $user['user_id'];
-        echo $userId;
+        $discoveredPeople = User::where('user_id', '!=', $userId)->get();
+
+        $discoveredPeople->each(function ($people) {
+            $id = $people->user_id;
+            $profileImage = ProfileImage::where('user_id', $id)->where('status', 1)->first();
+            $people->profile_image = $profileImage;
+        });
+
+        return response()->json($discoveredPeople);
     }
 }
--- next-social-media/src/app/discover-people/page.jsx
+++ next-social-media/src/app/discover-people/page.jsx
@@ -1,20 +1,30 @@
 "use client";
 import Header from "@/components/header";
-import SendFriendRequestSingle from "@/components/sendFriendRequestSingle";
 import SideMessages from "@/components/sideMessages";
 import Sidebar from "@/components/sidebar";
 import { useAuth } from "@/hooks/auth";
+import axios from "@/lib/axios";
+import useSWR from "swr";
 export default function DisCoverPeople() {
   const { user } = useAuth({ middleware: "auth" });
+  const { data: peoples } = useSWR("discover-people", () => axios.get("api/disocver-people"));
   return (
     <div>
       <Header />
       <section class="flex mt-2 bg-[#F0F2F5] gap-10 relative">
         <Sidebar user={user} />
         <div className=" w-2/4">
-          {Array.from({ length: 100 }, () => undefined).map(() => (
-            <SendFriendRequestSingle />
-          ))}
+          {peoples &&
+            peoples.data.map((people) => (
+              <div className="flex gap-3 items-center p-3">
+                <img className="w-12 h-12 object-cover rounded-full" src={process.env.NEXT_PUBLIC_BACKEND_URL + "/" + people.profile_image.image_path} alt="" />
+                <h3 className="font-medium text-lg">{people.user_first_name + " " + people.user_last_name}</h3>
+                <button className="bg-blue-600 text-white px-4 py-2 rounded-lg">
+                  Add Friend <i class="fa-solid fa-user-plus"></i>
+                </button>
+                <div className="bg-gray-300 text-black px-4 py-2 rounded-lg hidden">Request Sent</div>
+              </div>
+            ))}
         </div>
         <div className="w-1/4 bg-white flex flex-col items-end px-8 py-6 sticky top-20 h-full">
           <SideMessages />
