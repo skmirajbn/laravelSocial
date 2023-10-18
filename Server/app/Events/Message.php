e05e5e1f7c5a883c4e0359909e6dbdd15e4bee52
skmirajbn <skmirajbn@gmail.com>
2023-10-18 21:26:14 +0600

    updated

--- Server/app/Events/Message.php
+++ Server/app/Events/Message.php
@@ -2,18 +2,19 @@
 
 namespace App\Events;
 
-use BeyondCode\LaravelWebSockets\WebSockets\Channels\Channel;
+use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MessageSent implements ShouldBroadcastNow {
+class Message implements ShouldBroadcast {
     use Dispatchable, InteractsWithSockets, SerializesModels;
-    public $message;
+
     /**
      * Create a new event instance.
      */
+    public $message;
     public function __construct($message) {
         $this->message = $message;
     }
@@ -25,7 +26,7 @@ class MessageSent implements ShouldBroadcastNow {
      */
     public function broadcastOn(): array {
         return [
-            new Channel('messenger')
+            new Channel('message'),
         ];
     }
 }
--- Server/config/broadcasting.php
+++ Server/config/broadcasting.php
@@ -39,10 +39,12 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'host' => env('PUSHER_HOST'),
                 // 'host' => env('PUSHER_HOST') ?: 'api-' . env('PUSHER_APP_CLUSTER', 'mt1') . '.pusher.com',
-                'port' => env('PUSHER_PORT', 443),
+                'port' => env('PUSHER_PORT', 6001),
                 'scheme' => env('PUSHER_SCHEME', 'https'),
                 // 'encrypted' => true,
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                // 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                'useTLS' => true,
+                // 'encrypted' => true,
             ],
             'client_options' => [
                 // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
