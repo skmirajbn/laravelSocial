eb5f79476c55daa7abfb8cb332596b9b2cea47ec
skmirajbn <skmirajbn@gmail.com>
2023-10-21 19:14:20 +0600

    updated

--- Server/app/Http/Controllers/Friends/SearchFriendController.php
+++ Server/app/Http/Controllers/Friends/SearchFriendController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Friends;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
 class SearchFriendController extends Controller {
     function search($keywords) {
-        echo $keywords;
+        $users = User::where('user_first_name', 'like', '%' . $keywords . '%')
+            ->orWhere('email', 'like', '%' . $keywords . '%')
+            ->get();
+        return response()->json($users);
     }
 }
