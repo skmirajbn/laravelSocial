4f181aacba6f97d91ad53a23d1894ba0e71a41bd
skmirajbn <skmirajbn@gmail.com>
2023-10-21 23:52:57 +0600

    updated

--- next-social-media/src/app/messages/searchChats.jsx
+++ next-social-media/src/app/messages/searchChats.jsx
@@ -5,20 +5,23 @@ import { useRef, useState } from "react";
 export default function SearchChats() {
   const searchSuggestion = useRef();
   const [resultUsers, setResultUsers] = useState();
+  const [isSearching, setIsSearching] = useState(false);
   const handleOnFocus = () => {
     searchSuggestion.current.classList.remove("hidden");
   };
-  const handleOnBlue = () => {
-    // searchSuggestion.current.classList.add("hidden");
+  const handleOnBlur = () => {
+    searchSuggestion.current.classList.add("hidden");
   };
   let timeOutId;
   const handleSearch = async (e) => {
+    setIsSearching(true);
     clearTimeout(timeOutId);
-    let keyword = e.target.value.trim();
+    let keyword = e?.target?.value?.trim();
     let res;
     timeOutId = setTimeout(async () => {
       res = await axios.get(`api/search-friends/${keyword}`);
       setResultUsers(res.data);
+      setIsSearching(false);
     }, 500);
   };
   console.log(resultUsers);
@@ -35,16 +38,23 @@ export default function SearchChats() {
           </div>
         </div>
       </div>
-      <input type="text" placeholder="Search Chats" className="input input-bordered w-full max-w-xs rounded-full bg-gray-100 h-[2rem]" onFocus={handleOnFocus} onBlur={handleOnBlue} onChange={handleSearch} />
-      <div ref={searchSuggestion} className="absolute bg-gray-200 h-fit py-3 px-4 rounded-lg space-y-2">
-        {/* Single User */}
-        {resultUsers &&
-          resultUsers.map((user) => (
-            <div className="flex items-center gap-3 py-3 px-8 hover:bg-gray-300 rounded-lg">
-              <img className="w-9 h-9 object-cover rounded-full" src={process.env.NEXT_PUBLIC_BACKEND_URL + "/" + user.active_profile_image.image_path} alt="" />
-              <h3>{user.user_first_name + " " + user.user_last_name}</h3>
-            </div>
-          ))}
+      <div className="flex items-center gap-3">
+        <input type="text" placeholder="Search Chats" className="input input-bordered w-full max-w-xs rounded-full bg-gray-100 h-[2rem]" onFocus={handleOnFocus} onBlur={handleOnBlur} onChange={handleSearch} />
+        {isSearching && <span className="loading loading-bars loading-md"></span>}
+      </div>
+
+      <div ref={searchSuggestion} className="absolute">
+        {resultUsers && (
+          <div className="bg-gray-200 rounded-lg h-fit py-3 px-4  space-y-2">
+            {resultUsers &&
+              resultUsers.map((user) => (
+                <div className="flex items-center gap-3 py-3 px-8 hover:bg-gray-300 rounded-lg">
+                  <img className="w-9 h-9 object-cover rounded-full" src={process.env.NEXT_PUBLIC_BACKEND_URL + "/" + user.active_profile_image.image_path} alt="" />
+                  {user && <h3>{user.user_first_name + " " + user.user_last_name}</h3>}
+                </div>
+              ))}
+          </div>
+        )}
       </div>
       <h3 className="text-xl font-bold">Inbox</h3>
     </div>
