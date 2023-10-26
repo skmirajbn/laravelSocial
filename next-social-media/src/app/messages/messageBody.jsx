85de59a971802b88fa1164a856e47ae5ea01b9f9
skmirajbn <skmirajbn@gmail.com>
2023-10-26 21:09:04 +0600

    updated

--- next-social-media/src/app/messages/messageBody.jsx
+++ next-social-media/src/app/messages/messageBody.jsx
@@ -27,18 +27,17 @@ export default function MessageBody({ conversationuser, conversationID }) {
     }
   };
   useEffect(() => {
-    mutate();
+    mutate([]);
   }, [conversationID]);
   useEffect(() => {
     messageDiv.current.scrollTop = messageDiv.current.scrollHeight;
   }, []);
-  console.log(conversationuser);
   return (
     <div className="w-2/3 px-4" style={{ height: "calc(100vh - 5rem)" }}>
       <div className="h-24 py-4">
         <div className="flex items-center gap-4 justify-between">
           <div className="flex items-center gap-4">
-            <img className="h-14 w-14 object-cover rounded-full" src={process.env.NEXT_PUBLIC_BACKEND_URL + "/" + conversationuser?.active_profile_image?.image_path} alt="" />
+            {conversationuser && <img className="h-14 w-14 object-cover rounded-full" src={process.env.NEXT_PUBLIC_BACKEND_URL + "/" + conversationuser?.active_profile_image?.image_path} alt="" />}
             {conversationuser && <h3 className="text-xl font-bold">{conversationuser?.user_first_name + " " + conversationuser.user_last_name}</h3>}
           </div>
           <div className="flex gap-4 text-blue-600">
@@ -51,22 +50,32 @@ export default function MessageBody({ conversationuser, conversationID }) {
       </div>
       {/* Messages */}
       <div ref={messageDiv} className="overflow-y-auto py-4 px-3 rounded-lg bg-gradient-to-r from-emerald-50 to-blue-50" style={{ height: "calc(100% - 10rem)" }}>
-        <div className="flex flex-col gap-3 justify-end min-h-full">
-          {messages?.data?.map((message) => {
-            if (message?.user_id === user.user_id) {
-              return <MessageSender key={message.message_id} message={message?.message_text} user={message?.user} />;
-            } else {
-              return <MessageReceiver key={message.message_id} message={message?.message_text} user={message?.user} />;
-            }
-          })}
-        </div>
+        {!conversationuser && (
+          <div className=" h-full flex flex-col justify-center items-center gap-5">
+            <img className="w-60" src="img/select-conversation.svg" alt="" />
+            <h3 className="font-bold text-3xl bg-gradient-to-r from-emerald-400 to-blue-400 bg-clip-text text-transparent">No Convesation is selected</h3>
+          </div>
+        )}
+        {conversationuser && (
+          <div className="flex flex-col gap-3 justify-end min-h-full">
+            {messages?.data?.map((message) => {
+              if (message?.user_id === user.user_id) {
+                return <MessageSender key={message.message_id} message={message?.message_text} user={message?.user} />;
+              } else {
+                return <MessageReceiver key={message.message_id} message={message?.message_text} user={message?.user} />;
+              }
+            })}
+          </div>
+        )}
       </div>
       {/* Messging Writing */}
-      <div className="h-16 flex items-center gap-4">
-        <input type="text" placeholder="Write Message Here" className="input input-bordered w-full" value={message} onChange={(e) => setMessage(e.target.value)} onKeyUp={sendMessage} />
-        <i class="fa-solid fa-face-smile text-2xl"></i>
-        <i class="fa-solid fa-thumbs-up text-2xl"></i>
-      </div>
+      {conversationuser && (
+        <div className="h-16 flex items-center gap-4">
+          <input type="text" placeholder="Write Message Here" className="input input-bordered w-full" value={message} onChange={(e) => setMessage(e.target.value)} onKeyUp={sendMessage} />
+          <i class="fa-solid fa-face-smile text-2xl"></i>
+          <i class="fa-solid fa-thumbs-up text-2xl"></i>
+        </div>
+      )}
     </div>
   );
 }
--- next-social-media/src/app/messages/messageOptions.jsx
+++ next-social-media/src/app/messages/messageOptions.jsx
@@ -9,23 +9,33 @@ export default function MessageOptions({ conversationuser }) {
   const { data } = useSWR("profileImage", () => axios.get("/profile-image"));
 
   return (
-    <div className="w-1/3 py-4 space-y-4">
-      <img className="h-32 w-32 object-cover rounded-full mx-auto" src={process.env.NEXT_PUBLIC_BACKEND_URL + "/" + conversationuser?.active_profile_image?.image_path} alt="" />
-      {conversationuser && <h3 className="text-2xl text-center font-bold">{conversationuser?.user_first_name + " " + conversationuser?.user_last_name}</h3>}
-      <h5 className="text-center">
-        <i class="fa-solid fa-circle text-green-600"></i> Online
-      </h5>
-      <div className="flex justify-center gap-6 text-xl">
-        <div className="h-10 w-10 bg-gray-300 rounded-full flex items-center justify-center">
-          <i class="fa-solid fa-user"></i>
+    <div className="w-1/3">
+      {conversationuser && (
+        <div className="py-4 space-y-4">
+          <img className="h-32 w-32 object-cover rounded-full mx-auto" src={process.env.NEXT_PUBLIC_BACKEND_URL + "/" + conversationuser?.active_profile_image?.image_path} alt="" />
+          {conversationuser && <h3 className="text-2xl text-center font-bold">{conversationuser?.user_first_name + " " + conversationuser?.user_last_name}</h3>}
+          <h5 className="text-center">
+            <i class="fa-solid fa-circle text-green-600"></i> Online
+          </h5>
+          <div className="flex justify-center gap-6 text-xl">
+            <div className="h-10 w-10 bg-gray-300 rounded-full flex items-center justify-center">
+              <i class="fa-solid fa-user"></i>
+            </div>
+            <div className="h-10 w-10 bg-gray-300 rounded-full flex items-center justify-center">
+              <i class="fa-solid fa-volume-xmark"></i>
+            </div>
+            <div className="h-10 w-10 bg-gray-300 rounded-full flex items-center justify-center">
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </div>
+          </div>
         </div>
-        <div className="h-10 w-10 bg-gray-300 rounded-full flex items-center justify-center">
-          <i class="fa-solid fa-volume-xmark"></i>
+      )}
+      {!conversationuser && (
+        <div className="w-full h-full py-4 space-y-4 flex flex-col justify-center items-center ">
+          <img className="w-48" src="img/nothing-found.svg" alt="" />
+          <h3 className="text-2xl font-bold bg-gradient-to-r from-emerald-500 to-cyan-500 bg-clip-text text-transparent ">No Conversation is Selected</h3>
         </div>
-        <div className="h-10 w-10 bg-gray-300 rounded-full flex items-center justify-center">
-          <i class="fa-solid fa-magnifying-glass"></i>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
