e9e6313e0f5e23945e3a1feb58b69674dd426dca
skmirajbn <skmirajbn@gmail.com>
2023-10-28 18:11:22 +0600

    updated

--- next-social-media/src/app/user/[username]/layout.jsx
+++ next-social-media/src/app/user/[username]/layout.jsx
@@ -38,12 +38,12 @@ export default function UserProfileLayout({ children }) {
         </div>
         <hr className="mt-3" />
         <div className="flex px-16 py-2 gap-4 text-xl font-medium text-gray-700 border-b border-t border-gray-400">
-          <Link className="text-blue-600" href="/profile">
+          <Link className="text-blue-600" href={`/user/${userInfo?.user_username}`}>
             Posts
           </Link>
           <a href="">About</a>
           <a href="">Friends</a>
-          <Link href="/profile/photos">Photos</Link>
+          <Link href={`/user/${userInfo?.user_username}/photos`}>Photos</Link>
           <a href="">Groups</a>
         </div>
         {children}
---
+++ next-social-media/src/app/user/[username]/photos/page.jsx
@@ -0,0 +1,3 @@
+export default function userPhotos() {
+  return <div>userPhotos</div>;
+}
